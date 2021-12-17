8f80656e4a01ec56af3fe04d12028445f16ed5a5
jetmaquiling <jet.maquiling@gmail.com>
2021-12-17 13:49:41 +0800

    donation update

--- pages/info/donate.js
+++ pages/info/donate.js
@@ -12,8 +12,14 @@ export default function Program() {
       <HeadV2 backdrop={true}/>
         <div className={styles.mainContainer}>
             <h1 className={styles.title}>Donate</h1>
+
             <div className={styles.detailBox}>
-              <p className={styles.note}>Ernie Abella Advocacy and Candidacy is supported by Bagong Pilipino = Bagong Pilipinas, a non-profit and non-government organization, duly registered at the Securities and Exchange Commission.</p>
+              <p className={styles.note}>Be a part of the fulfillment of the Destiny & Calling of the nation Philippines i.e. to   be a "Light to the Nations". We value your prayers and monetary support. Please send your cash donations to below account. Please accept our sincere thanks for your contribution.</p>
+            </div>
+
+
+            <div className={styles.detailBox}>
+              <p className={styles.note}>Bagong Pilipino = Bagong Pilipinas, -- a non-profit and non-government organization, duly registered at the Securities and Exchange Commission -- is the official support organization of ERNIE ABELLA.</p>
             </div>
             
             <div className={styles.detailBox}>
