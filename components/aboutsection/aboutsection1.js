54833bfdba0762b8916e9474632f28e93be9856d
jetmaquiling <jet.maquiling@gmail.com>
2022-01-17 14:27:16 +0800

    about Section Change

--- components/aboutsection/aboutsection1.js
+++ components/aboutsection/aboutsection1.js
@@ -3,7 +3,13 @@ import Head from 'next/head'
 import * as style from '@/styles/aboutsection/aboutsection1.module.css'
 import Link from 'next/link'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import StopIcon from '@material-ui/icons/Stop';
 
+const about = [
+   "ERNIE ABELLA, publicly known as SPOX, was the first Spokesperson of President Rodrigo Duterte.",
+   "After fulfilling his mandate at Malacanang, he moved to the Department of Foreign Affairs as Undersecretary for Strategic Communications, representing the Philippines internationally at various diplomatic, democratic, cultural, political and religious fora; and in inter-agency and inter-branch activities of government.",
+   "He also served as Officer-in-Charge of the UNESCO National Commission (NATCOM) working closely with the Creative Bloc of Congress, efforts of toward promoting Philippine Creative Cities, and advancing UN peace efforts through Educational, Scientific and Cultural cooperation."
+]
 
 const bio = [
    "A FOUNDATIONAL Builder.",
@@ -13,28 +19,30 @@ const bio = [
 ]
 
 const government = [
-   "Former DFA Undersecretary for Strategic Communications on 2017 until 2021 October.",
-   "The First and former Spokesperson of President Rodrigo Roa Duterte in 2016",
-   "Officer-in-charge of UNESCO National Commission",
+   "He started as junior copywriter in Manila; & became Creative Director 5 years later",
+   "Taught at the Ateneo de Davao Humanities & Communication Arts Dept",
+   "He started and was pastor of a successful church for 18 years",
+   "Founder/president of an innovative secondary school & for 20 years",
+   "Founder/chair of a thriving credit cooperative for 18 years Sent missionaries to Southeast Asia and East Asia"
 ]
 
 const business = [
-   "A SOCIAL ENTREPRENEUR for 19 years.",
-   "A Leader who works with the CIVIL SOCIETY from 1988 until Present.",
-   "A FOUNDATIONAL Builder.",
-   "A Father figure to Many.",
-   "20 Years as an Educator - Founder, and Former President of The Southpoint School, Davao",
-   "20 Years in Cooperative Founder, and Former Chairman of One Accord Cooperative, Davao - this coop started from 10,000 pesos now , Millions in Assets, has helped a multitude of Start-Ups from Davao to Luzon and Visayas"
+   "Graduated pre-medicine from Ateneo de Davao",
+   "Studied at the Graduate Schools of both English and Communication Arts Department of Ateneo de Manila",
+   "Finished his Master in Divinity, Silliman University",
+   "Earned his Master in Entrepreneurship (Social Development) Asian Institute of Management (A.I.M.)"
 ]
 
 const academic = [
-   "Masters in Entrepreneurship at Asian Institute of Management (AIM) - Social Entrepreneurship.",
-   "Masters of Divinity at Siliman University - Cumlaude",
-   "Studied Communication Arts at the Ateneo de Manila Graduate School",
-   "Graduated, AB Pre-Med at the Ateneo de Davao"
+   "He learns from social media, vloggers, bloggers, young people, kids and those who take the opposite view.",
+   "Ernie has a wife, Joy and 2 kids who have birthed, 6 healthy children - with one on the way.",
+   "He is also a father figure, and a spiritual mentor and counselor to many ordinary men and women, along with many who have their own ministries."
 ]
 
-
+const quote = [
+   'He Says, "it is my experience with people, from Makati to the provinces, from schools to farms, from credit cooperatives to churches, or government, that made me see and realize, that there is a genius within people that knows the solution of the problems they face, they just need to be heard. When we change mindset, when we change inside and listen to one another, then miracles happen."',
+   '"Pag nabago ang PILIPINO mababago ang PILIPINAS"'
+]
 
 
 
@@ -52,9 +60,11 @@ const AboutSection1 = () => {
              <div  className={style.contentBox}>
                 <h6  className={style.title}>ABOUT</h6>
                 <div  className={style.underline}/>
-                <div  className={style.textBox}>Ernesto "Ernie" Corpuz Abella is a Filipino businessman, writer, and former evangelist who served in President Rodrigo Duterte's administration as Presidential Spokesperson (2016-2017) and Undersecretary for Strategic Communications and Research of the Department of Foreign Affairs (2017-2021).</div>
-                <div  className={style.textBox}>Before joining the government, he was an executive with a Davao city-based agricultural products manufacturer and a local cooperative which he co-founded. He also established a school in Davao and was a columnist for local newspaper Sunstar Davao.</div>
-                     
+                  {about.map((data,index)=>{
+                     return (
+                        <div key={index}  className={style.textBox}>{data}</div>
+                     )
+                  })}
              </div>
 
              
@@ -66,12 +76,12 @@ const AboutSection1 = () => {
 
             <div  className={style.container2}>
                 <div  className={style.contentBox2}>
-                    <h6  className={style.title}>Background in Governance</h6>
+                    <h6  className={style.title}>A Builder of Institutions</h6>
                     <div  className={style.underline2}/>
                         {government.map((data,index)=>{
                            return (
                               <div key={index} className={style.textContainer}>
-                                 <FiberManualRecordIcon style={{color: '#fff'}}/>
+                                 <StopIcon  fontSize="small" style={{color: '#fff'}}/>
                                  <p className={style.textItem}>{data}</p>
                               </div> 
                            )
@@ -82,12 +92,12 @@ const AboutSection1 = () => {
 
             <div  className={style.container2}>
                 <div  className={style.contentBox2}>
-                    <h6  className={style.title}>Business Experience</h6>
+                    <h6  className={style.title}>Understands & Appreciates both Formal & Self-Education</h6>
                     <div  className={style.underline2}/>
                         {business.map((data,index)=>{
                            return (
                               <div key={index} className={style.textContainer}>
-                                 <FiberManualRecordIcon style={{color: '#fff'}}/>
+                                 <StopIcon fontSize="small"  style={{color: '#fff'}}/>
                                  <p className={style.textItem}>{data}</p>
                               </div> 
                            )
@@ -95,14 +105,12 @@ const AboutSection1 = () => {
                 </div>
             </div>
 
+
             <div  className={style.container2}>
-                <div  className={style.contentBox2}>
-                    <h6  className={style.title}>Education</h6>
-                    <div  className={style.underline2}/>
+                <div  className={style.contentBox3}>
                         {academic.map((data,index)=>{
                            return (
                               <div key={index} className={style.textContainer}>
-                                 <FiberManualRecordIcon style={{color: '#fff'}}/>
                                  <p className={style.textItem}>{data}</p>
                               </div> 
                            )
@@ -112,20 +120,12 @@ const AboutSection1 = () => {
 
 
             <div  className={style.container2}>
-                <div  className={style.contentBox2}>
-                    <h6  className={style.title}>BIO</h6>
-                    <div  className={style.underline2}/>
-                     
-                           {bio.map((data,index)=>{
-                              return (
-                                 <div key={index} className={style.textContainer}>
-                                    <FiberManualRecordIcon style={{color: '#fff'}}/>
-                                    <p className={style.textItem}>{data}</p>
-                                 </div> 
-                              )
-                           })}
-                           
-                     
+                <div  className={style.contentBox4}>
+                        {quote.map((data,index)=>{
+                           return (
+                                 <p className={style.textItem}>{data}</p>
+                           )
+                        })}
                 </div>
             </div>
             
