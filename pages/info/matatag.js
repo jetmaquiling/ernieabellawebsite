ee8da4759c1f63e74627fc49656f4066a691ea58
jetmaquiling <jet.maquiling@gmail.com>
2022-02-06 08:47:05 +0800

    Matatag Na Bayan

--- pages/info/matatag.js
+++ pages/info/matatag.js
@@ -15,7 +15,23 @@ export default function Patas() {
       <div className={style.container}>
         <div className={style.maincontent}>
           <h2>Matatag Na Bayan</h2>
+          <p>
+            Sa tulong nang kabataan ay itataguyod ni ERNIE ABELLA ang bayang
+            matatag at handang manilbihan sa kapwa.. Ang National Volunteer
+            Corps (NaVoCor) ay isang proyekto kung saan ay palalaganapin ni
+            ERNIE ABELLA ang malawakang “Bayanihan” sa buong bansa. Ilan sa
+            magiging aktibidad nang NaVoCor ay tree planting, paglilinis nang
+            mga ilog at batis, pagsasanay sa ilalim nang Red Cross nang iba’t-
+            ibang “survival techniques”, at pag-aaral kung paano maibabalik ang
+            isang syudad na nasalanta nang sakuna, sa dati. Ang NaVoCor ay
+            magsisilbing lakas nang bayan sa bawat lindol, bagyo, land-slide,
+            sunog, baha at kung anu-ano pang sakuna na sama-sama nating
+            haharapin na naka-kapitbisig at walang iwanan! Ang National
+            Volunteer Corps ay magiging patunay sa sinasabi ni ERNIE ABELLA na
+            kaakibat nang pagtutulungan ay isang MATATAG NA BAYAN!
+          </p>
         </div>
+
         <div className={style.buttonContainer}>
           <Link href="/info/gobyerno">
             <h6>Gobyernong Nararamdaman</h6>
