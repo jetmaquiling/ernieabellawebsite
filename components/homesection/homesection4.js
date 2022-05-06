65de5ab8f66353508be0ac8f1791ffd36107a4b6
jetmaquiling <jet.maquiling@gmail.com>
2022-05-06 09:13:43 +0800

    Ernie Abella

--- components/homesection/homesection4.js
+++ components/homesection/homesection4.js
@@ -1,57 +1,75 @@
-import * as React from "react"
-import Head from 'next/head'
-import * as style from '@/styles/homesection/homesection4.module.css'
-import Link from 'next/link'
-import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
-import qs from 'qs'
-import config from '@/config/configuration.json';
-import moment from 'moment';
-import axios from 'axios';
+import * as React from "react";
+import Head from "next/head";
+import * as style from "@/styles/homesection/homesection4.module.css";
+import Link from "next/link";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import qs from "qs";
+import config from "@/config/configuration.json";
+import moment from "moment";
+import axios from "axios";
 import NewSection1 from "../newsection/newsection1";
 
-
 const HomeSection4 = () => {
-    const [mainArticle, setMainArticle] = React.useState({})
-    const [feature, setFeature] = React.useState([])
+  const [mainArticle, setMainArticle] = React.useState({});
+  const [feature, setFeature] = React.useState([]);
 
-    React.useEffect(() => {
-        async function getBlog() { 
-            try{
-                const query = qs.stringify({ _where: { _or:[{"section_position": "feature"},{"section_position": "main"}]} });
-                const {data} = await axios.get(`${config.SERVER_URL}/article-ernie-abellas?${query}`);
+  React.useEffect(() => {
+    async function getBlog() {
+      try {
+        const query = qs.stringify({
+          _where: {
+            _or: [
+              { section_position: "feature" },
+              { section_position: "main" },
+            ],
+          },
+        });
+        const { data } = await axios.get(
+          `${config.SERVER_URL}/article-ernie-abellas?${query}`
+        );
 
-                data.map((article,index)=>{
-                    if(article.section_position === "main"){
-                        setMainArticle(data[index])
-                        setMainArticle({...data[index], 
-                            clipboard: data[index].thumbnail_image.url, 
-                        })
-                    }
+        data.map((article, index) => {
+          if (article.section_position === "main") {
+            setMainArticle(data[index]);
+            setMainArticle({
+              ...data[index],
+              clipboard: data[index].thumbnail_image.url,
+            });
+          }
 
-                    if(article.section_position === "feature"){
-                        setFeature(feature => [...feature, data[index]]);
-                    }
-                })
-                
-            }catch(err){
-                console.log(err)
-            }
-            
-            
-        }
-        getBlog()
-
-    }, [])
-
+          if (article.section_position === "feature") {
+            setFeature((feature) => [...feature, data[index]]);
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getBlog();
+  }, []);
 
   return (
-        <div className={style.main}>
-            <h2 className={style.title}>News and Updates</h2>
-            <NewSection1/>
-            <div className={style.buttonContainer} ><Link href="/blog"><h4 className={style.button}>See More </h4></Link></div>
-        </div>
-  )
-  
-}
+    <div className={style.main}>
+      <h2 className={style.title}>News and Updates</h2>
+      <div className={style.videoContainer}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Yj2YM-RdSUo"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <NewSection1 />
+      <div className={style.buttonContainer}>
+        <Link href="/blog">
+          <h4 className={style.button}>See More </h4>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default HomeSection4
+export default HomeSection4;
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import FooterV1 from '@/components/footer/footerv1'
-import FooterV2 from '@/components/footer/footerv2'
-import HeadV2 from '@/components/head/headv2'
-import HomeSection1 from '@/components/homesection/homesection1'
-import HomeSection2 from '@/components/homesection/homesection2'
-import HomeSection3 from '@/components/homesection/homesection3'
-import HomeSection4 from '@/components/homesection/homesection4'
-import Youtube from '@/components/homesection/youtube'
-import CarouselComponent from '@/components/homesection/carousel'
-import NewSection1 from '@/components/newsection/newsection1'
-import Head from 'next/head'
-import styles from './home.module.css'
+import React from "react";
+import FooterV1 from "@/components/footer/footerv1";
+import FooterV2 from "@/components/footer/footerv2";
+import HeadV2 from "@/components/head/headv2";
+import HomeSection1 from "@/components/homesection/homesection1";
+import HomeSection2 from "@/components/homesection/homesection2";
+import HomeSection3 from "@/components/homesection/homesection3";
+import HomeSection4 from "@/components/homesection/homesection4";
+import Youtube from "@/components/homesection/youtube";
+import CarouselComponent from "@/components/homesection/carousel";
+import NewSection1 from "@/components/newsection/newsection1";
+import Head from "next/head";
+import styles from "./home.module.css";
 
 export default function Home() {
-  const [openVideo , setOpenVideo] = React.useState({url:'none', open: false})
-
+  const [openVideo, setOpenVideo] = React.useState({ url: "none", open: true });
 
   return (
     <div className={styles.main}>
-       <Head>
-          <title>ERNIE ABELLA - BAGONG PILIPINAS</title>
-          <meta name="description" content="PAG NABAGO ANG LOOB NG BAWAT PILIPINO MABABAGO ANG BAYAN NG PILIPINAS"/>
-          <meta property="og:type"               content="website" />
-          <meta property="og:title"              content="ERNIE ABELLA - BAGONG PILIPINAS" />
-          <meta property="og:image"              content='./Thumbnail/ernieabella.png' />
-        </Head>
-      
-      <HeadV2/>
-      <Youtube openVideo={openVideo} setOpenVideo={setOpenVideo}/>
-      <CarouselComponent  openVideo={openVideo} setOpenVideo={setOpenVideo}  />
-      <HomeSection2/>
-      <HomeSection3/>
-      <HomeSection4/>
-      <FooterV1/>
-      <FooterV2/>
+      <Head>
+        <title>ERNIE ABELLA - BAGONG PILIPINAS</title>
+        <meta
+          name="description"
+          content="PAG NABAGO ANG LOOB NG BAWAT PILIPINO MABABAGO ANG BAYAN NG PILIPINAS"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="ERNIE ABELLA - BAGONG PILIPINAS" />
+        <meta property="og:image" content="./Thumbnail/ernieabella.png" />
+      </Head>
+
+      <HeadV2 />
+      <Youtube openVideo={openVideo} setOpenVideo={setOpenVideo} />
+      <CarouselComponent openVideo={openVideo} setOpenVideo={setOpenVideo} />
+      <HomeSection2 />
+      <HomeSection3 />
+      <HomeSection4 />
+      <FooterV1 />
+      <FooterV2 />
     </div>
-  )
+  );
 }
